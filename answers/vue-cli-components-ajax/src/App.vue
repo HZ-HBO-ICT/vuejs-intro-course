7bc24ab756210f091dc60c9df1e31be62cbe2418
Rimmert Zelle <rimmertzelle@192.168.68.104>
2019-06-05 21:38:22 +0200

    added the ajax answers

---
+++ answers/vue-cli-components-ajax/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Album v-for="album in albums" v-bind:key="album.id" v-bind:album="album"></Album>
+  </div>
+</template>
+
+<script>
+const axios = require("axios");
+import Album from "./components/Album.vue";
+
+export default {
+  name: "app",
+  components: { Album },
+  data() {
+    return {
+      albums: []
+    };
+  },
+  mounted() {
+    axios
+      .get("./data/albums.json")
+      .then(response => (this.albums = response.data));
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ answers/vue-cli-components-ajax/src/components/Song.vue
@@ -0,0 +1,12 @@
+<template>
+  <tr><td>{{song.id}}</td><td>{{song.title}}</td><td>{{song.duration}}</td></tr>
+</template>
+
+<script>
+export default {
+  name: "song",
+  props: ["song"]
+}
+</script>
+
+<style></style>
